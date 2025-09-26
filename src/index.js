68d28f2fe395b8e940e9c1eba187e0fe499af581
Rui Nabeshima <ruinabeshima0321@gmail.com>
2025-09-26 10:07:23 +0900

    Can fetch data from API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
 import "./styles.css"
+
+document.addEventListener("DOMContentLoaded", () => {
+  weatherAPI("london")
+  weatherAPI("madrid")
+  weatherAPI("iwea")
+})
+
+
+async function weatherAPI(location){
+  try {
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=76PJ9K5RSV4XR2D96MQUVYZKV`)
+    const data = await response.json()
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
